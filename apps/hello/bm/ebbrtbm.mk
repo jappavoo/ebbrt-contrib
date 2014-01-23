86f35ce2a807948ecd6bde26096111d377b8906d
Jonathan Appavoo <jappavoo@bu.edu>
2014-01-23 16:41:36 -0500

    My first  cut and an out of source EbbRT baremetal app

---
+++ apps/hello/bm/ebbrtbm.mk
@@ -0,0 +1,13 @@
+ifeq ($(strip ${EBBRT_SRCDIR}),)
+ $(error You must set EBBRT_SRCDIR to the path of an EbbRT source tree)
+endif	
+src = ${EBBRT_SRCDIR}
+VPATH = ${EBBRT_SRCDIR}:../../src
+
+ifeq ($(strip ${EBBRT_OPTFLAGS}),)
+ $(error You must set EBBRT_OPTFLAGS as you need. eg. -g3 -O4 -flto -DNDEBUG for an optimized build or -g3 -O0 for a non-optimized debug build)
+endif	
+
+optflags = ${EBBRT_OPTFLAGS}
+include ${EBBRT_SRCDIR}/build.mk
+
---
+++ apps/hello/bm/src/hello.cpp
@@ -0,0 +1,8 @@
+#include <sys/debug.hpp>
+
+using namespace ebbrt;
+
+void appmain()
+{
+  kprintf("Hello World!!!\n"); 
+}
