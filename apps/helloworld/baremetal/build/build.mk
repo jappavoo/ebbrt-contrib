5103fbea49b7fa0c6ef9b9aa876fe10f185c023b
Jonathan Appavoo <jappavoo@bu.edu>
2014-03-12 21:53:07 -0400

    missing files and directories

---
+++ apps/helloworld/baremetal/build/build.mk
@@ -0,0 +1,4 @@
+EBBRT_APP_OBJECTS := helloworld.o Printer.o
+EBBRT_TARGET := helloworld
+
+include $(abspath ../../../../ebbrtbaremetal.mk)
---
+++ apps/helloworld/hosted/build/build.mk
@@ -0,0 +1,7 @@
+app_sources := \
+	helloworld.cc \
+	Printer.cc 
+
+target := helloworld
+
+include $(abspath ../../../../ebbrthosted.mk)
